7a0cdca8ea4a84cd0e4ad54037732064c92b9b3e
Fauzan Amir Al Ghiffary <56854433+alghiffaryfa19@users.noreply.github.com>
2025-03-18 18:20:26 +0700

    update

--- jade-vendor.mk
+++ jade-vendor.mk
@@ -1219,3 +1219,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     vendor/jingpad/jade/proprietary/vendor/etc/vintf/manifest/manifest_soter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/vintf/manifest/manifest_soter.xml \
     vendor/jingpad/jade/proprietary/vendor/etc/vintf/manifest/vendor.sprd.hardware.fingerprintmmi@1.0-service.xml:$(TARGET_COPY_OUT_VENDOR)/etc/vintf/manifest/vendor.sprd.hardware.fingerprintmmi@1.0-service.xml \
     vendor/jingpad/jade/proprietary/vendor/etc/vintf/manifest/vendor.sprd.hardware.gnss@2.0-service.xml:$(TARGET_COPY_OUT_VENDOR)/etc/vintf/manifest/vendor.sprd.hardware.gnss@2.0-service.xml \
+
+PRODUCT_PACKAGES += \
+    libstagefright_codecbase \
+    libstagefright_wfd
